69a0f2315fd9a9543c46f751ca5e4ef8cffed400
lUCI <lusindisontanjana@gmail.com>
2022-05-28 11:49:15 +0200

    use invlovement API to get and post likes, display likes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import './styles/main.scss';
 import 'regenerator-runtime/runtime.js';
 import render from './modules/render.js';
 import changeHandler from './modules/changeHandler.js';
+import getLikes from './modules/getLikes';
+import eventHandler from './modules/eventHandler';
 
 const getData = async () => {
   const res = await fetch(
     'https://www.themealdb.com/api/json/v1/1/filter.php?c=beef',
   );
   const data = await res.json();
-  render(data.meals);
+  const likes = await getLikes();
+  render(data.meals, likes);
 };
 
 getData();
 changeHandler();
+eventHandler();
--- src/modules/render.js
+++ src/modules/render.js
@@ -3,22 +3,28 @@ import itemsCounter from './itemsCounter.js';
 const main = document.querySelector('.main');
 const heading = document.querySelector('.heading');
 
-const render = (data) => {
+const render = (data, likesList) => {
   heading.innerHTML = `Meals(${itemsCounter(data)})`;
   main.innerHTML = '';
   data.forEach((el) => {
-    main.innerHTML += (
-      `<div class="card">
+    likesList.forEach((like) => {
+			if (like.item_id === el.idMeal) {
+				el.likes = like.likes;
+			}
+		});
+    main.innerHTML += `<div class="card" id="${el.idMeal}">
 <img src="${el.strMealThumb}" alt="" class="card__img" />
 <div class="card__top">
 <p class="card__top--name">${el.strMeal}</p>
-<div class="card__top--like"><i class="fa-regular fa-heart"></i> likes</div>
+<div class="card__top--like"><i class="fa-solid fa-heart" id='like'></i><p> ${
+			el.likes || ""
+		} </p>likes</div>
 </div>
 <div class="card__bottom">
-<a href="" class="btn">Comment</a>
-<a href="" class="btn">Resevation</a>
+<a href="" id="comments" class="btn">Comment</a>
+<a href="" id="reservations" class="btn">Resevation</a>
 </div>
-</div>`);
+</div>`;
   });
 };
 
